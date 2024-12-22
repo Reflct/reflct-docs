abeb88a623239b78518d0146d218c13f718e95bc
William Falloon <william.falloon@NZ2DDBM0VJPXGY.local>
2024-12-22 22:39:46 +1300

    Added react library docs

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -3,6 +3,7 @@ export default {
   reflct_welcome_guide: "Getting started",
   views: "Creating views",
   Optimizing_3DGS_for_Web: "Optimizing 3DGS for Web",
+  Integration_React_library: "Integration: React library",
   about: {
     title: "About",
     type: "page"
